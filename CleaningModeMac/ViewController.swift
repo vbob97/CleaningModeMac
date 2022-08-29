9faea5d6d40ce0efdf3b093fd505e25b53c883d3
vbob97 <vincenzo.bombace@studenti.unicampania.it>
2022-08-29 16:07:11 +0200

    Initial Commit

---
+++ CleaningModeMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CleaningModeMac
+//
+//  Created by Vincenzo Bombace on 29/08/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
